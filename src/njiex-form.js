4b06aa195dc082e4e1cf5b12b9c54bf251d356b0
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2022-07-05 16:29:09 +0200

    drag

--- src/njiex-form.js
+++ src/njiex-form.js
@@ -112,6 +112,7 @@ function allowDrop(ev) {
 
 function performanceDrag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.dropEffect = "move";
 }
 
 function performanceDrop(ev) {
