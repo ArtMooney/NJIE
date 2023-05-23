eee08353a5ad84c646978e8dc80444895002b32a
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-05-23 13:48:16 +0200

    small changes

--- src/index-language-selector-propud.js
+++ src/index-language-selector-propud.js
@@ -1,14 +1,12 @@
 const chosenlanguage = sessionStorage.getItem("chosenlanguage");
 
 (async function pullGeo() {
+  const domainUrl = "https://www.propud.com";
+
   const res = await fetch(
     "https://api.ipdata.co/?api-key=a9301d2e756fd26a8a81a0f3041a5863feaa213787072c5366a25140"
   );
 
-  if (res.status !== 200) {
-    console.log("error");
-    window.location.href = "https://www.propud.com";
-  }
   const data = await res.json();
 
   const url = window.location.hostname.split(".").pop().toLowerCase();
@@ -28,23 +26,11 @@ const chosenlanguage = sessionStorage.getItem("chosenlanguage");
   }
 
   if (inviteExists === false) {
-    const domainUrl = "https://www.propud.com";
-
     // test domain url's first and then country code
     if (url === "it") {
       window.location.href = domainUrl + "/it";
     } else if (url === "se") {
       window.location.href = domainUrl + "/sv";
-    } else if (url === "no") {
-      window.location.href = domainUrl;
-    } else if (url === "dk") {
-      window.location.href = domainUrl;
-    } else if (url === "fi") {
-      window.location.href = domainUrl;
-    } else if (url === "es") {
-      window.location.href = domainUrl;
-    } else if (url === "is") {
-      window.location.href = domainUrl;
     } // no domains matched, check for country codes
     else if (data.country_code === "SE") {
       sessionStorage.setItem("chosenlanguage", data.country_code);
@@ -52,24 +38,6 @@ const chosenlanguage = sessionStorage.getItem("chosenlanguage");
     } else if (data.country_code === "IT") {
       sessionStorage.setItem("chosenlanguage", data.country_code);
       window.location.href = domainUrl + "/it";
-    } else if (data.country_code === "FI") {
-      sessionStorage.setItem("chosenlanguage", data.country_code);
-      window.location.href = domainUrl;
-    } else if (data.country_code === "NO") {
-      sessionStorage.setItem("chosenlanguage", data.country_code);
-      window.location.href = domainUrl;
-    } else if (data.country_code === "DK") {
-      sessionStorage.setItem("chosenlanguage", data.country_code);
-      window.location.href = domainUrl;
-    } else if (data.country_code === "ES") {
-      sessionStorage.setItem("chosenlanguage", data.country_code);
-      window.location.href = domainUrl;
-    } else if (data.country_code === "IS") {
-      sessionStorage.setItem("chosenlanguage", data.country_code);
-      window.location.href = domainUrl;
-    } else {
-      // go to fallback url if nothing matches
-      window.location.href = domainUrl;
     }
   }
 })();
